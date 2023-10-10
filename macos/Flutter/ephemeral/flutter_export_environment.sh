ba5c7b9be2463d3e8b0eba21cf924b363a1f3e3a
Jakob Körber <56073945+jakobkoerber@users.noreply.github.com>
2023-10-10 22:10:39 +0200

    configure mac os app

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.10.5/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jakobkoerber/Developer/TUM/TCA/Campus-Flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/jakobkoerber/Developer/TUM/TCA/Campus-Flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.2.1"
+export "FLUTTER_BUILD_NUMBER=0.2.1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNzk0Y2YyNjgxYzZjOWZlN2IyNjBlMGU4NGRlOTYyOThkYzljMThiLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/jakobkoerber/Developer/TUM/TCA/Campus-Flutter/.dart_tool/package_config.json"
